4f60df05f1def537930949c5d339c4415372b679
ainis997 <235386176+ainis997@users.noreply.github.com>
2026-05-18 14:17:25 +0300

    Sukurta pirminė primityvi programos versija.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,62 @@
-//
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <iomanip>
+#include <exception>
+
+class Tekstas
+{
+    std::string ivesties_failo_pav;
+    std::unordered_map<std::string, unsigned int> zodziai;
+
+public:
+    Tekstas(std::string ivesties_failo_pav) : ivesties_failo_pav(ivesties_failo_pav) {}
+
+    void suskaityt_zodziai()
+    {
+        std::ifstream failas(ivesties_failo_pav);
+        std::string eil;
+        if (!failas.is_open())
+        {
+            throw std::runtime_error("Nepavyko atidaryti failo: " + ivesties_failo_pav);
+        }
+
+        unsigned int eil_num = 0;
+        while (std::getline(failas, eil))
+        {
+            std::istringstream sr(eil);
+            std::string zodis;
+            while (sr >> zodis)
+            {
+                zodziai[zodis]++; // jei žodžio nėra, bus pridėtas naujas raktas su default int verte 0, ir po ++ jis taps 1; jei žodis yra, tsg bus ++'intas dab skaitiklis
+            }
+            ++eil_num;
+        }
+        failas.close();
+    }
+
+    void spausd_zodziu_statistika(std::string isvesties_failo_pav = "isvestis.txt")
+    {
+        std::ofstream isvesties_failas(isvesties_failo_pav);
+        for (const auto &pora : zodziai)
+        {
+            isvesties_failas << std::left << std::setw(30) << pora.first << std::left << std::setw(10) << pora.second << '\n';
+        }
+        isvesties_failas.close();
+    }
+
+    // friend std::ostream &operator<<(std::ostream &os, const std::unordered_map<std::string, unsigned int> &zodziai)
+    // {
+    //     //
+    // }
+};
+
+int main()
+{
+    Tekstas tekstas("tekstas.txt");
+    tekstas.suskaityt_zodziai();
+    tekstas.spausd_zodziu_statistika();
+    return 0;
+}
